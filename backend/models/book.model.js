10a665961b03c9b4c4a265db25ff450bf9992836
Darshini S <darshini-s@t0xr76m6w6-mac.lan>
2024-07-02 14:23:36 +0530

    my forst commit

---
+++ backend/models/book.model.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, Sequelize, DataTypes) => {
+    const Book = sequelize.define(
+      "book", // Model name
+      {
+        // Model attributes
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true
+        },
+        title: {
+          type: DataTypes.STRING
+        },
+        author_id: {
+            type: DataTypes.INTEGER
+        },
+        genre_id: {
+            type: DataTypes.INTEGER
+        },
+        published: {
+          type: DataTypes.BOOLEAN
+        },
+        publication_date: {
+            type: DataTypes.DATE
+        },
+        language: {
+            type: DataTypes.STRING
+        },
+        price: {
+            type: DataTypes.DECIMAL(8,2)
+        },
+        created_at: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updated_at: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
+      },
+      {
+        // Options
+        timestamps: true,
+        underscrored: true,
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+      }
+    );
+  
+    return Book;
+  };

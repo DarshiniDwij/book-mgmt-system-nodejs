5f3ca116f4f4e24ecdfc6ee18ae93e6855de95c6
darshini-s <darshini-s@pluralsight.com>
2024-07-02 22:39:54 +0530

    added server.js code

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const config = require("./config/config.js")
+
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// database
+const db = require("./models");
+
+db.sequelize.sync().then(() => {
+    initial(); 
+  });
+
+  // simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Hi there, welcome to book management system" });
+  });
+
+
+  // api routes
+  require("./routes/book.routes")(app)
+  require("./routes/author.routes")(app)
+  require("./routes/genre.routes")(app)
+
+
+// set port, listen for requests
+const PORT = config.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
